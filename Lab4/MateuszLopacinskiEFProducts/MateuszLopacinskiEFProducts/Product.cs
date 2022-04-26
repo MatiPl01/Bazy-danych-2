de81e5502ac2ac63298b88863f74e4f44e985951
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2022-04-26 09:22:18 +0200

    Finished preparation tasks from lab4

---
+++ Lab4/MateuszLopacinskiEFProducts/MateuszLopacinskiEFProducts/Product.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MateuszLopacinskiEFProducts
+{
+    internal class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int UnitsOnStock { get; set; } 
+    }
+}
